ec25091e6012dfd1bd22a6c1f7f5e8f30977b873
as199 <Dioneassane0290@gmail.com>
2020-08-21 17:06:30 +0000

    reference

--- src/Entity/Referenciel.php
+++ src/Entity/Referenciel.php
@@ -10,8 +10,49 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
- * @ORM\Entity(repositoryClass=ReferencielRepository::class)
- * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ReferencielRepository", repositoryClass=ReferencielRepository::class)
+ * @ApiResource(
+ *     collectionOperations={
+ *     "get_referenciel"={
+ *     "method"="GET",
+ *     "normalizationContext"={"groups":{"referenciel:read","referenciel:read_all"}},
+ *     "path"="/admin/referentiels",
+ *      "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *     },
+ *     "get_groupe_competence"={
+ *     "method"="GET",
+ *     "normalizationContext"={"groups":{"grcreferenciel:read","referenciel:read_all"}},
+ *     "path"="/admin/referentiels/grpecompetences",
+ *     "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *     },
+ *     "post_referenciel"={
+ *     "method"="POST",
+ *     "path"="/admin/referentiels",
+ *     "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM'))"
+ *     },
+ *     "post_groupe_competence"={
+ *     "method"="POST",
+ *     "denormalizationContext"={"groups":{"gecreferenciel:write"}},
+ *     "path"="/admin/referentiels/grpecompetences",
+ *     }
+ *     },itemOperations={
+ *     "GET"={
+ *           "path":"/admin/referentiels/{id}",
+ *     "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM') or is_granted('ROLE_APPRENANT')"
+ *     },
+ *     "GET"={
+ *           "path":"/admin/referentiels/grpecompetences/{id}",
+ *           "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM') or is_granted('ROLE_APPRENANT')"
+ *     },
+ *     "PUT"={
+ *          "path":"/admin/referentiels/id/{id}",
+ *          "access_control"="(is_granted('ROLE_ADMIN') or is_granted('ROLE_FORMATEUR') or is_granted('ROLE_CM') or is_granted('ROLE_APPRENANT')"
+ *     }
+ * }
+ *
+ *
+ *
+ * )
  */
 class Referenciel
 {
@@ -19,12 +60,14 @@ class Referenciel
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"referenciel:read", "gecreferenciel:write","grcreferenciel:read"})
 
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     *  @Groups({"referenciel:read","grcreferenciel:read","gecreferenciel:write","referenciel:read_all","apprenant:read","suprime:read","formateur:read","admin:read"})
 
      */
     private $libelle;
@@ -36,6 +79,7 @@ class Referenciel
 
     /**
      * @ORM\ManyToMany(targetEntity=GroupeCompetence::class, mappedBy="referentiels")
+     * @Groups({"grcreferenciel:read",})
      */
     private $groupeCompetences;
 
